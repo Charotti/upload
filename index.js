2deaa0c6857aaf8fb4a38237d314f56447c5842b
unknown <clamy6@yahoo.fr>
2022-03-16 15:24:57 +0100

    image post

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const port = 8000;
-
+const upload = multer({ dest: "public/uploads" });
 app.use(express.static("public"));
+
+app.post("/user", upload.single("image"), (req, res) => {
+  fs.renameSync(
+    req.file.path,
+    path.join(req.file.destination, req.file.originalname)
+  );
+
+  res.send("Image received");
+});
 
 app.listen(port, () => {
   console.log("Server started on port: " + port);
